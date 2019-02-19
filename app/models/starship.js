a3834821525a0f4a0142729042d867f87ce7e0bc
Clair Rogers <clair-rogers@outlook.com>
2019-02-19 09:35:25 -0700

    added hyperdrive and speed

--- app/models/starship.js
+++ app/models/starship.js
@@ -5,6 +5,8 @@ export default class Starship {
     this.cost_in_credits = data.cost_in_credits
     this.crew = data.crew
     this.passengers = data.passengers
+    this.speed = data.speed || data.max_atmosphering_speed
+    this.hyperdrive = data.hyperdrive || data.hyperdrive_rating
     this.url = data.url
   }
   getBasicTemplate() {
@@ -17,6 +19,8 @@ export default class Starship {
       <p>Cost: ${this.cost_in_credits} credits</p>
       <p>Crew: ${this.crew} crewmembers</p>
       <p>Passengers: ${this.passengers} people</p>
+      <p>Max Speed: ${this.speed} km/p</p>
+      <p>Hyperdrive rating: ${this.hyperdrive}</p>
     `
   }
 }
